8cf4a5e227fc961f7d661d95cd78e9702c001349
sjkdenny <sjkdenny@gmail.com>
2018-03-03 13:09:21 -0800

    added new field for the bed file default in the pipeline for 'gene_type'

--- puflibs/variables.py
+++ puflibs/variables.py
@@ -1,3 +1,3 @@
 
 bed_fields = ['chrm', 'start', 'stop', 'name', 'score', 'strand']
-motif_fields_additional = ['annotation', 'refseq_id', 'exon']
+motif_fields_additional = ['annotation', 'refseq_id', 'exon', 'gene_type']
